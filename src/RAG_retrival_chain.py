fd5b6b4c3fd83783c035bd5a0a6c864625beb6ad
moiz-mansoori <moizroshan2003@gmail.com>
2025-12-09 18:18:15 +0500

    Fix LangChain 1.0+ import: use langchain.chains.retrieval submodule

--- src/RAG_retrival_chain.py
+++ src/RAG_retrival_chain.py
@@ -1,6 +1,6 @@
 # src/RAG_retrival_chain.py
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
